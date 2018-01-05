d74612a4aa6e8cd676e890d76a46e35726b6d29a
zitro <bryan@bryanortiz.com>
2018-01-05 08:59:22 -0500

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,21 @@
 # Write your code here!
+require 'pry'
 
 
+def good_practices
+  game_hash.each do |location, team_data|
+    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+    binding.pry
+      team_data.each do |attribute, data|
+        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+        binding.pry
 
+        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+        data.each do |data_item|
+            binding.pry
+      end
+    end
+  end
+end
 
-
-
-
-
-
-
+good_practices
